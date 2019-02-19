353bdb4979cfd21f97c60fec46eca7d6ad9b9029
shenxianhui <shenxh@chint.com>
2019-02-19 17:04:26 +0800

    feat: 商品详情: 增加商品规格弹出框

--- src/views/goods/Goods.vue
+++ src/views/goods/Goods.vue
@@ -2,7 +2,7 @@
  * @Author: ShenXianhui 
  * @Date: 2019-02-15 14:40:03 
  * @Last Modified by: ShenXianhui
- * @Last Modified time: 2019-02-15 16:43:44
+ * @Last Modified time: 2019-02-19 16:59:56
  */
 <!-- 商品详情 -->
 <template>
@@ -69,7 +69,7 @@
                 </div>
                 <div class="goods-info">
                     <div class="buy">
-                        <div class="select">
+                        <div class="select" @click="getSku()">
                             <div class="group">
                                 <span>已选</span>
                                 <p>小米8 屏幕指纹版 8GB+128GB 透明下x1</p>
@@ -112,6 +112,62 @@
                 @click="getGoods"
             />
         </van-goods-action>
+        <!-- 商品规格 -->
+        <div class="sku">
+            <van-popup
+                v-model="isSku"
+                position="bottom"
+                :overlay="true">
+                <div class="container">
+                    <div class="sku-details">
+                        <img :src="imgSrc" alt="商品">
+                        <div class="title">
+                            <div class="price">
+                                <span>￥3399</span>
+                                <del>￥3599</del>
+                            </div>
+                            <p>小米8 屏幕指纹版 8GB+128GB 透明</p>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <div class="parameter">
+                            <h4>{{ specifications.title }}</h4>
+                            <div class="parameter-select">
+                                <span
+                                    :class="specificationsIndex === index ? 'active' : ''"
+                                    v-for="(item, index) in specifications.option"
+                                    :key="item.id"
+                                    @click="getParameter(item.value, index, 'specifications')">
+                                    {{ item.label }}
+                                </span>
+                            </div>
+                        </div>
+                        <div class="parameter">
+                            <h4>{{ color.title }}</h4>
+                            <div class="parameter-select">
+                                <span
+                                    :class="colorIndex === index ? 'active' : ''"
+                                    v-for="(item, index) in color.option"
+                                    :key="item.id"
+                                    @click="getParameter(item.value, index, 'color')">
+                                    {{ item.label }}
+                                </span>
+                            </div>
+                        </div>
+                        <div class="quantity">
+                            <h4>购买数量</h4>
+                            <van-stepper
+                                v-model="stepperNum"
+                                :integer="isInteger"
+                                :disable-input="isDisable" />
+                        </div>
+                    </div>
+                </div>
+                <div class="add-cart">
+                    加入购物车
+                </div>
+            </van-popup>
+        </div>
     </div>
 </template>
 
@@ -127,6 +183,40 @@ export default {
     data() {
         return {
             placeholder: '请输入内容', // 搜索框提示
+            isSku: false, // 商品规格弹出框
+            imgSrc: require('@/images/common/mi_phone_1.png'), // 商品规格-顶部图片
+            stepperNum: 1, // 当前输入值
+            isInteger: true, // 是否只允许输入整数
+            isDisable: true, // 是否禁用输入框
+            specificationsIndex: 0, // 商品规格-参数索引
+            colorIndex: 0, // 商品规格-颜色索引
+
+            specifications: { // 商品规格-规格
+                title: '规格',
+                option: [
+                    {
+                        label: '8GB+128GB',
+                        value: '8128'
+                    },
+                    {
+                        label: '6GB+128GB',
+                        value: '6128'
+                    }
+                ]
+            },
+            color: { // 商品规格-颜色
+                title: '颜色',
+                option: [
+                    {
+                        label: '红色',
+                        value: 'red'
+                    },
+                    {
+                        label: '黑色',
+                        value: 'black'
+                    }
+                ]
+            }
         };
     },
     computed: {},
@@ -136,6 +226,26 @@ export default {
         // 喜欢此商品
         getLike() {
             console.log('喜欢');
+        },
+
+        // 商品规格选择
+        getSku() {
+            this.isSku = true;
+        },
+
+        // 点击遮罩层
+        setOverlay() {
+            this.isSku = false;
+        },
+
+        // 商品规格-参数选择
+        getParameter(value, index, type) {
+            if (type === 'specifications') {
+                this.specificationsIndex = index;
+            } else {
+                this.colorIndex = index;
+            }
+            console.log(value);
         },
 
         // 进入购物车
@@ -228,9 +338,13 @@ export default {
                 }
             }
         }
+        .goods-parameter::-webkit-scrollbar { /* 隐藏滚动条, 这种方式不兼容 火狐 和 IE */
+            display: none;
+        }
         .goods-info {
             width: 100%;
             padding: 0 0.15rem;
+            margin-top: 0.1rem;
             .buy {
                 width: 100%;
                 border-radius: 0.1rem;
@@ -303,6 +417,118 @@ export default {
             height: 0.45rem;
             line-height: 0.45rem;
             background-color: #ff6700;
+        }
+    }
+    /* 商品规格弹出框 */
+    .sku {
+        /deep/ .van-popup {
+            width: 100%;
+            height: 70%;
+            border-radius: 0.06rem 0.06rem 0 0;
+            overflow: hidden;
+            .container {
+                height: 100%;
+                .sku-details {
+                    position: fixed;
+                    top: 0;
+
+                    display: flex;
+
+                    width: 100%;
+                    height: 1.3rem;
+                    padding: 0.15rem;
+                    background-color: #fff;
+                    border-bottom: 1px solid #eee;
+                    z-index: 9;
+                    img {
+                        width: 1rem;
+                        height: 1rem;
+                        border: 1px solid #ddd;
+                        border-radius: 0.03rem;
+                    }
+                    .title {
+                        display: flex;
+                        justify-content: center;
+                        flex-direction: column;
+
+                        width: 100%;
+                        padding-left: 0.05rem;
+                        .price {
+                            width: 100%;
+                            span {
+                                font-size: 0.25rem;
+                                color: #ff6700;
+                            }
+                            del {
+                                font-size: 0.14rem;
+                                color: #666;
+                            }
+                        }
+                        p {
+                            font-size: 0.13rem;
+                        }
+                    }
+                }
+                .content {
+                    height: 100%;
+                    padding: 1.3rem 0.15rem 0.42rem;
+                    overflow: auto;
+                    .parameter {
+                        padding: 0.1rem 0 0.1rem;
+                        border-bottom: 1px solid #eee;
+                        h4 {
+                            font-weight: normal;
+                            font-size: 0.16rem;
+                        }
+                        .parameter-select {
+                            display: flex;
+                            flex-wrap: wrap;
+                            span {
+                                padding: 0.06rem;
+                                border-radius: 0.03rem;
+                                border: 1px solid #ccc;
+                                margin-top: 0.08rem;
+                                &:not(:last-child) {
+                                    margin-right: 0.08rem;
+                                }
+                            }
+                            .active {
+                                color: #ff6700;
+                                border-color: #ff6700;
+                            }
+                        }
+                    }
+                    .quantity {
+                        display: flex;
+                        justify-content: space-between;
+                        align-items: center;
+
+                        padding: 0.1rem 0 0.1rem;
+                        h4 {
+                            font-weight: normal;
+                            font-size: 0.16rem;
+                            margin-bottom: 0.1rem;
+                        }
+                    }
+                }
+                .content::-webkit-scrollbar { /* 隐藏滚动条, 这种方式不兼容 火狐 和 IE */
+                    display: none;
+                }
+            }
+            .add-cart {
+                position: fixed;
+                bottom: 0;
+
+                width: 100%;
+                height: 0.42rem;
+                text-align: center;
+                line-height: 0.42rem;
+                font-size: 0.16rem;
+                background-color: #ff6700;
+                color: #fff;
+
+                z-index: 9;
+            }
         }
     }
 }
